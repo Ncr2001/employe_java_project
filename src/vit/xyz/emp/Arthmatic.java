371caff8bbd5fead0bc6d7d299a88c4fad70e0c4
Ncr2001 <chandanansr2001@gmail.com>
2023-12-03 20:54:32 +0530

    added the Arthmatic files

---
+++ src/vit/xyz/emp/Arthmatic.java
@@ -0,0 +1,13 @@
+package vit.xyz.emp;
+
+public class Arthmatic {
+
+	public static void main(String[] args) {
+	    int a =20;
+		int b=10;
+        System.out.println("A+B="+(a+b));
+        System.out.println("A-B="+(a-b));
+        System.out.println("A*B="+(a*b));
+	}
+
+}
