dba11f879cd5f3639606cea8c2e0880fdabcc085
Ncr2001 <chandanansr2001@gmail.com>
2023-12-03 20:59:39 +0530

    remove spaces

--- src/vit/xyz/emp/Arthmatic.java
+++ src/vit/xyz/emp/Arthmatic.java
@@ -5,9 +5,8 @@ public class Arthmatic {
 	public static void main(String[] args) {
 	    int a =20;
 		int b=10;
-        System.out.println("A+B="+(a+b));
-        System.out.println("A-B="+(a-b));
-        System.out.println("A*B="+(a*b));
-	}
-
+        System.out.println("a - b="+(a-b));
+        System.out.println("a * b="+(a*b));
+        System.out.println("a + b="+(a+b));
+        }
 }
